0890108027225b5773f0893b3ea430ecf3941fea
Semilogo Bamidele <emmanbamidele471@gmail.com>
2022-09-06 00:19:38 +0100

    Added the Search filter and Region Dropdown

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-// import Header from "./Components/Header";
+// import Forms from "./Components/Forms";
 import CountryPage from "./Pages/CountryPage";
 
 function App() {
   const [country, setCountry] = useState([]);
   const [darkMode, setDarkMode] = useState(false);
+  const [country0, setCountry0] = useState([]);
+  const [region0, setRegion0] = useState("Filter by Region");
+
+  const onChangeHandler = (value) => {
+    if (value) {
+      let placeholder = [...country0];
+      placeholder = placeholder.filter((place) =>
+        place.name.official.toLowerCase().includes(value.toLowerCase())
+      );
+      setCountry(placeholder);
+      return;
+    }
+
+    setCountry(country0);
+  };
+
+  const regionSwitch = (value) => {
+    if (value) {
+      let dropdown = [...region0];
+      dropdown = dropdown.filter((drop) =>
+        drop.region.toLowerCase().includes(value.toLowerCase())
+      );
+      setCountry(dropdown);
+      return;
+    }
+
+    setCountry(region0);
+  };
 
   useEffect(() => {
     getCountry();
@@ -15,6 +43,8 @@ function App() {
     const api = await fetch("https://restcountries.com/v3.1/all");
     const data = await api.json();
     setCountry(data);
+    setCountry0(data);
+    setRegion0(data);
     console.log(data);
   };
   return (
@@ -26,13 +56,40 @@ function App() {
           </div>
           <div className="theme_mode">
             <p className="app_theme" onClick={() => setDarkMode(!darkMode)}>
-              <i class="fa-regular fa-moon"></i>Dark Mode
+              <i className="fa-regular fa-moon"></i>
+              {darkMode ? "Light" : "Dark"} Mode
             </p>
           </div>
         </div>
+        <div className="forms">
+          <form className="app_search">
+            <input
+              type="text"
+              placeholder="Search for a Country"
+              className="search_input"
+              onInput={(e) => onChangeHandler(e.target.value)}
+            />
+          </form>
+          <form className="region_section">
+            <label htmlFor="Country Regions">
+              Filter by Regions:
+              <select
+                className="app_regions"
+                onChange={(e) => regionSwitch(e.target.value)}
+              >
+                {/* <option>Filter by Regions</option> */}
+                <option>Africa</option>
+                <option>America</option>
+                <option>Asia</option>
+                <option>Europe</option>
+                <option>Oceania</option>
+              </select>
+            </label>
+          </form>
+        </div>
         <div className="app_country">
           {country.map((item) => {
-            return <CountryPage item={item} />;
+            return <CountryPage item={item} key={item.ccn3} />;
           })}
         </div>
       </div>
