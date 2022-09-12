d92220b9882792b93dbb2fa865c27e3d4f81ccff
Semilogo Bamidele <emmanbamidele471@gmail.com>
2022-09-13 00:01:47 +0100

    started with the details component.

--- src/App.js
+++ src/App.js
@@ -1,100 +1,16 @@
-import { useEffect, useState } from "react";
-import "./App.css";
-import CountryPage from "./Pages/CountryPage";
+import React from "react";
+import Home from "./Pages/Home";
+import { Routes, Route } from "react-router-dom";
+import Details from "./Pages/Details";
 
 function App() {
-  const [country, setCountry] = useState([]);
-  const [darkMode, setDarkMode] = useState(false);
-  const [country0, setCountry0] = useState([]);
-  const [region0, setRegion0] = useState("Filter by Region");
-
-  const onChangeHandler = (value) => {
-    if (value) {
-      let placeholder = [...country0];
-      placeholder = placeholder.filter((place) =>
-        place.name.official.toLowerCase().includes(value.toLowerCase())
-      );
-      setCountry(placeholder);
-      return;
-    }
-
-    setCountry(country0);
-  };
-
-  const regionSwitch = (value) => {
-    if (value) {
-      let dropdown = [...region0];
-      dropdown = dropdown.filter((drop) =>
-        drop.region.toLowerCase().includes(value.toLowerCase())
-      );
-      setCountry(dropdown);
-      return;
-    }
-
-    setCountry(region0);
-  };
-
-  useEffect(() => {
-    getCountry();
-  }, []);
-
-  const getCountry = async () => {
-    const api = await fetch("https://restcountries.com/v3.1/all");
-    const data = await api.json();
-    setCountry(data);
-    setCountry0(data);
-    setRegion0(data);
-    console.log(data);
-  };
   return (
-    <div className={darkMode ? "dark-mode" : "light-mode"}>
-      <div className="App">
-        <div className="header">
-          <div className="header_txt">
-            <a href="/">
-              <h2>Where in the World?</h2>
-            </a>
-          </div>
-          <div className="theme_mode">
-            <p className="app_theme" onClick={() => setDarkMode(!darkMode)}>
-              <i className="fa-regular fa-moon"></i>
-              {darkMode ? "Light" : "Dark"} Mode
-            </p>
-          </div>
-        </div>
-        <div className="forms">
-          <form className="app_search">
-            <input
-              type="text"
-              placeholder="Search for a Country"
-              className="search_input"
-              onInput={(e) => onChangeHandler(e.target.value)}
-            />
-          </form>
-          <form className="region_section">
-            <label htmlFor="Country Regions">
-              Filter by Regions:
-              <select
-                className="app_regions"
-                onChange={(e) => regionSwitch(e.target.value)}
-              >
-                {/* <option>Filter by Regions</option> */}
-                <option>Africa</option>
-                <option>America</option>
-                <option>Asia</option>
-                <option>Europe</option>
-                <option>Oceania</option>
-              </select>
-            </label>
-          </form>
-        </div>
-        <div className="app_country">
-          {country.map((item) => {
-            return <CountryPage item={item} key={item.ccn3} />;
-          })}
-        </div>
-      </div>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/details" element={<Details />} />
+      </Routes>
+    </>
   );
 }
 
--- src/Pages/CountryPage.js
+++ src/Pages/CountryPage.js
@@ -1,34 +1,39 @@
 import React from "react";
 import "./CountryPage.css";
+import { Link } from "react-router-dom";
 
 function CountryPage({ item }) {
   return (
-    <div className="countries">
-      <div className="countries_content">
-        <img
-          src={item.flags.png}
-          alt={item.name.common}
-          className="countries_img"
-        />
-        <div className="countries_content_details">
-          <h2 className="country_name">{item.name.official}</h2>
-          <div className="countries_details">
-            <p className="countries_detail">
-              Population:
-              <span className="country_details_answers">{item.population}</span>
-            </p>
-            <p className="countries_detail">
-              Region:
-              <span className="country_details_answers">{item.region}</span>
-            </p>
-            <p className="countries_detail">
-              Capital:
-              <span className="country_details_answers">{item.capital}</span>
-            </p>
+    <Link to="/details" className="text-link">
+      <div className="countries">
+        <div className="countries_content">
+          <img
+            src={item.flags.png}
+            alt={item.name.common}
+            className="countries_img"
+          />
+          <div className="countries_content_details">
+            <h2 className="country_name">{item.name.official}</h2>
+            <div className="countries_details">
+              <p className="countries_detail">
+                Population:
+                <span className="country_details_answers">
+                  {item.population}
+                </span>
+              </p>
+              <p className="countries_detail">
+                Region:
+                <span className="country_details_answers">{item.region}</span>
+              </p>
+              <p className="countries_detail">
+                Capital:
+                <span className="country_details_answers">{item.capital}</span>
+              </p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 }
 
