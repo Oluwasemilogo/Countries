cfa9679e2cf3b10b109e4750c852015919d3ebb1
Semilogo Bamidele <emmanbamidele471@gmail.com>
2022-09-18 16:54:40 +0100

    added some styling

--- src/Pages/Details.js
+++ src/Pages/Details.js
@@ -1,9 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Details() {
   return (
     <div>
       <h1>Hello world</h1>
+      <Link to="/" className="return-link">
+        <div className="return-btn">
+          <button>Back</button>
+        </div>
+      </Link>
     </div>
   );
 }
