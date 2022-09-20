196b9dfa861a344bdea54a0e008834925274d815
Semilogo Bamidele <emmanbamidele471@gmail.com>
2022-09-21 00:57:00 +0100

    working on the details page

--- src/Pages/Details.js
+++ src/Pages/Details.js
@@ -3,11 +3,13 @@ import { useContext } from "react";
 import { useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
 import { ThemeContext } from "./Context";
+import "./Details.css";
+import Header from "./Header";
 
 function Details() {
   let { country } = useParams();
   const [details, setDetails] = useState([]);
-  const { country0 } = useContext(ThemeContext);
+  const { country0, isToggle } = useContext(ThemeContext);
 
   useEffect(() => {
     const data = country0.filter(
@@ -17,25 +19,56 @@ function Details() {
   }, [country0]);
 
   return (
-    <div>
-      <h1>Hello world</h1>
+    <div className={`details ${isToggle ? "light-mode" : "dark-mode"}`}>
+      <Header />
       <Link to="/" className="return-link">
-        <div className="return-btn">
-          <button>Back</button>
+        <div>
+          <button
+            className={`return-btn ${isToggle ? "light-mode" : "dark-mode"}`}
+          >
+            <i className="fa-solid fa-arrow-left"></i> Back
+          </button>
         </div>
       </Link>
       <div className="details_content">
-        <h3>{details?.name?.common}</h3>
-        <img
-          src={details?.flags?.png}
-          alt=""
-          style={{ height: "260px", width: "250px" }}
-        ></img>
-        <img
-          src={details?.coatOfArms?.png}
-          alt=""
-          style={{ height: "50px", width: "50px" }}
-        ></img>
+        <img src={details?.flags?.png} alt="" className="details_img"></img>
+        <div className="details_txt_content">
+          <h3 className="details_name">{details?.name?.official}</h3>
+          <div className="other">
+            <div className="other1">
+              <h4 className="details_other1_content">
+                Native name:{details?.name?.common}
+              </h4>
+              <h4 className="details_other1_content">
+                Population:{details?.population}
+              </h4>
+              <h4 className="details_other1_content">
+                Region:{details?.region}
+              </h4>
+              <h4 className="details_other1_content">
+                Sub Region:{details?.subregion}
+              </h4>
+              <h4 className="details_other1_content">
+                Capital:{details?.capital}
+              </h4>
+            </div>
+
+            <div className="other2">
+              <h4 className="details_other1_content">
+                Top Level Domain:{details?.tld}
+              </h4>
+              <h4 className="details_other1_content">
+                Currencies:{details?.currencies?.name}
+              </h4>
+              {/* <h4 className="details_other1_content">
+                Languages:{details?.languages}
+              </h4> */}
+            </div>
+          </div>
+          <div className="details_border">
+            <h4>Border Countries:</h4>
+          </div>
+        </div>
       </div>
     </div>
   );
